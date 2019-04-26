966de40041d53ba4ee9cebcc5b9cda736c8fd42c
13701138894 <13701138894@163.com>
2019-04-27 01:21:33 +0800

    lst 4.26代码及备份

--- Dior-lipstick/top-lst/js/top.js
+++ Dior-lipstick/top-lst/js/top.js
@@ -1,26 +1,26 @@
 (function () {
     var pics = [
-        { src: '1.jpg', tSrc: '1-t.jpg', lSrc: '1-l.jpg', rSrc: '1-r.jpg', diff: '1-diff.png',cSrc:'1-c.png',sSrc:'1-s.jpg'},
-        { src: '2.jpg', tSrc: '2-t.jpg', lSrc: '2-l.jpg', rSrc: '2-r.jpg', diff: '2-diff.png',cSrc:'2-c.png',sSrc:'2-s.jpg'},
-        { src: '3.jpg', tSrc: '3-t.jpg', lSrc: '3-l.jpg', rSrc: '3-r.jpg', diff: '3-diff.png',cSrc:'3-c.png',sSrc:'3-s.jpg'},
-        { src: '4.jpg', tSrc: '4-t.jpg', lSrc: '4-l.jpg', rSrc: '4-r.jpg', diff: '4-diff.png',cSrc:'4-c.png',sSrc:'4-s.jpg'},
-        { src: '5.jpg', tSrc: '5-t.jpg', lSrc: '5-l.jpg', rSrc: '5-r.jpg', diff: '5-diff.png',cSrc:'5-c.png',sSrc:'5-s.jpg'},
-        { src: '6.jpg', tSrc: '6-t.jpg', lSrc: '6-l.jpg', rSrc: '6-r.jpg', diff: '6-diff.png',cSrc:'6-c.png',sSrc:'6-s.jpg'},
-        { src: '7.jpg', tSrc: '7-t.jpg', lSrc: '7-l.jpg', rSrc: '7-r.jpg', diff: '7-diff.png',cSrc:'7-c.png',sSrc:'7-s.jpg'},
-        { src: '8.jpg', tSrc: '8-t.jpg', lSrc: '8-l.jpg', rSrc: '8-r.jpg', diff: '8-diff.png',cSrc:'8-c.png',sSrc:'8-s.jpg'},
-        { src: '9.jpg', tSrc: '9-t.jpg', lSrc: '9-l.jpg', rSrc: '9-r.jpg', diff: '9-diff.png',cSrc:'9-c.png',sSrc:'9-s.jpg'},
-        { src: '10.jpg', tSrc: '10-t.jpg', lSrc: '10-l.jpg', rSrc: '10-r.jpg', diff: '10-diff.png' ,cSrc:'10-c.png',sSrc:'10-s.jpg'},
-        { src: '11.jpg', tSrc: '11-t.jpg', lSrc: '11-l.jpg', rSrc: '11-r.jpg', diff: '11-diff.png' ,cSrc:'11-c.png',sSrc:'11-s.jpg'},
-        { src: '12.jpg', tSrc: '12-t.jpg', lSrc: '12-l.jpg', rSrc: '12-r.jpg', diff: '12-diff.png' ,cSrc:'12-c.png',sSrc:'12-s.jpg'},
-        { src: '13.jpg', tSrc: '13-t.jpg', lSrc: '13-l.jpg', rSrc: '13-r.jpg', diff: '13-diff.png' ,cSrc:'13-c.png',sSrc:'13-s.jpg'},
-        { src: '14.jpg', tSrc: '14-t.jpg', lSrc: '14-l.jpg', rSrc: '14-r.jpg', diff: '14-diff.png' ,cSrc:'14-c.png',sSrc:'14-s.jpg'},
-        { src: '15.jpg', tSrc: '15-t.jpg', lSrc: '15-l.jpg', rSrc: '15-r.jpg', diff: '15-diff.png' ,cSrc:'15-c.png',sSrc:'15-s.jpg'},
-        { src: '16.jpg', tSrc: '16-t.jpg', lSrc: '16-l.jpg', rSrc: '16-r.jpg', diff: '16-diff.png' ,cSrc:'16-c.png',sSrc:'16-s.jpg'},
-        { src: '17.jpg', tSrc: '17-t.jpg', lSrc: '17-l.jpg', rSrc: '17-r.jpg', diff: '17-diff.png' ,cSrc:'17-c.png',sSrc:'17-s.jpg'},
-        { src: '18.jpg', tSrc: '18-t.jpg', lSrc: '18-l.jpg', rSrc: '18-r.jpg', diff: '18-diff.png' ,cSrc:'18-c.png',sSrc:'18-s.jpg'},
-        { src: '19.jpg', tSrc: '19-t.jpg', lSrc: '19-l.jpg', rSrc: '19-r.jpg', diff: '19-diff.png' ,cSrc:'19-c.png',sSrc:'19-s.jpg'},
-        { src: '20.jpg', tSrc: '20-t.jpg', lSrc: '20-l.jpg', rSrc: '20-r.jpg', diff: '20-diff.png' ,cSrc:'20-c.png',sSrc:'20-s.jpg'},
-        { src: '21.jpg', tSrc: '21-t.jpg', lSrc: '21-l.jpg', rSrc: '21-r.jpg', diff: '21-diff.png' ,cSrc:'21-c.png',sSrc:'21-s.jpg'}
+        { src: '1.jpg', tSrc: '1-t.jpg', lSrc: '1-l.jpg', rSrc: '1-r.jpg', diff: '1-diff.png', cSrc: '1-c.png', sSrc: '1-s.jpg' },
+        { src: '2.jpg', tSrc: '2-t.jpg', lSrc: '2-l.jpg', rSrc: '2-r.jpg', diff: '2-diff.png', cSrc: '2-c.png', sSrc: '2-s.jpg' },
+        { src: '3.jpg', tSrc: '3-t.jpg', lSrc: '3-l.jpg', rSrc: '3-r.jpg', diff: '3-diff.png', cSrc: '3-c.png', sSrc: '3-s.jpg' },
+        { src: '4.jpg', tSrc: '4-t.jpg', lSrc: '4-l.jpg', rSrc: '4-r.jpg', diff: '4-diff.png', cSrc: '4-c.png', sSrc: '4-s.jpg' },
+        { src: '5.jpg', tSrc: '5-t.jpg', lSrc: '5-l.jpg', rSrc: '5-r.jpg', diff: '5-diff.png', cSrc: '5-c.png', sSrc: '5-s.jpg' },
+        { src: '6.jpg', tSrc: '6-t.jpg', lSrc: '6-l.jpg', rSrc: '6-r.jpg', diff: '6-diff.png', cSrc: '6-c.png', sSrc: '6-s.jpg' },
+        { src: '7.jpg', tSrc: '7-t.jpg', lSrc: '7-l.jpg', rSrc: '7-r.jpg', diff: '7-diff.png', cSrc: '7-c.png', sSrc: '7-s.jpg' },
+        { src: '8.jpg', tSrc: '8-t.jpg', lSrc: '8-l.jpg', rSrc: '8-r.jpg', diff: '8-diff.png', cSrc: '8-c.png', sSrc: '8-s.jpg' },
+        { src: '9.jpg', tSrc: '9-t.jpg', lSrc: '9-l.jpg', rSrc: '9-r.jpg', diff: '9-diff.png', cSrc: '9-c.png', sSrc: '9-s.jpg' },
+        { src: '10.jpg', tSrc: '10-t.jpg', lSrc: '10-l.jpg', rSrc: '10-r.jpg', diff: '10-diff.png', cSrc: '10-c.png', sSrc: '10-s.jpg' },
+        { src: '11.jpg', tSrc: '11-t.jpg', lSrc: '11-l.jpg', rSrc: '11-r.jpg', diff: '11-diff.png', cSrc: '11-c.png', sSrc: '11-s.jpg' },
+        { src: '12.jpg', tSrc: '12-t.jpg', lSrc: '12-l.jpg', rSrc: '12-r.jpg', diff: '12-diff.png', cSrc: '12-c.png', sSrc: '12-s.jpg' },
+        { src: '13.jpg', tSrc: '13-t.jpg', lSrc: '13-l.jpg', rSrc: '13-r.jpg', diff: '13-diff.png', cSrc: '13-c.png', sSrc: '13-s.jpg' },
+        { src: '14.jpg', tSrc: '14-t.jpg', lSrc: '14-l.jpg', rSrc: '14-r.jpg', diff: '14-diff.png', cSrc: '14-c.png', sSrc: '14-s.jpg' },
+        { src: '15.jpg', tSrc: '15-t.jpg', lSrc: '15-l.jpg', rSrc: '15-r.jpg', diff: '15-diff.png', cSrc: '15-c.png', sSrc: '15-s.jpg' },
+        { src: '16.jpg', tSrc: '16-t.jpg', lSrc: '16-l.jpg', rSrc: '16-r.jpg', diff: '16-diff.png', cSrc: '16-c.png', sSrc: '16-s.jpg' },
+        { src: '17.jpg', tSrc: '17-t.jpg', lSrc: '17-l.jpg', rSrc: '17-r.jpg', diff: '17-diff.png', cSrc: '17-c.png', sSrc: '17-s.jpg' },
+        { src: '18.jpg', tSrc: '18-t.jpg', lSrc: '18-l.jpg', rSrc: '18-r.jpg', diff: '18-diff.png', cSrc: '18-c.png', sSrc: '18-s.jpg' },
+        { src: '19.jpg', tSrc: '19-t.jpg', lSrc: '19-l.jpg', rSrc: '19-r.jpg', diff: '19-diff.png', cSrc: '19-c.png', sSrc: '19-s.jpg' },
+        { src: '20.jpg', tSrc: '20-t.jpg', lSrc: '20-l.jpg', rSrc: '20-r.jpg', diff: '20-diff.png', cSrc: '20-c.png', sSrc: '20-s.jpg' },
+        { src: '21.jpg', tSrc: '21-t.jpg', lSrc: '21-l.jpg', rSrc: '21-r.jpg', diff: '21-diff.png', cSrc: '21-c.png', sSrc: '21-s.jpg' }
     ];
     var datas = [
         { color: '白色', num: '000 Ultra light 47', price: '320.00' },
@@ -60,7 +60,7 @@
         `
         $('<li class="sc"></li>').appendTo('.color ul').html(src)
     }
-    var sy=0;
+    var sy = 0;
     $('.color li').on('click', function () {
         $(this).addClass('qq').siblings().removeClass('qq');
         var index = $(this).index();
@@ -107,7 +107,7 @@
         $('.color li').eq(sy).find('span').css('border-color', '#000').end().siblings().find('span').css('border-color', 'transparent');
         //改变en描述
         $('.up .ms .en').text(datas[sy].num);
-        
+
         $('.up .ms .cn').text(obj2.color);
         //up图片链接更改
         var src = obj1.src;
@@ -149,9 +149,9 @@
         $('.up .ms .cn').text(txt2);
         $('.up .jage').text(txt3);
         $('.up img').attr('src', src);
-        sy=$(this).index();
+        sy = $(this).index();
         $('.color li').eq($(this).index()).find('span').css('border-color', '#000')
-        .end().siblings().find('span').css('border-color', 'transparent');
+            .end().siblings().find('span').css('border-color', 'transparent');
         $('.color li').eq($(this).index()).addClass('qq').siblings().removeClass('qq');
         var obj1 = pics[$(this).index()];
         var obj2 = datas[$(this).index()];
@@ -178,14 +178,14 @@
     function juanQu() {
         var h1 = $(window).scrollTop();
         // 回到顶部按钮出现与隐藏
-        if(h1>=100){
-            $('.toTop').show().css('z-index',10000);
-            
-        }else {
+        if (h1 >= 100) {
+            $('.toTop').show().css('z-index', 10000);
+
+        } else {
             $('.toTop').hide();
         }
         // 点击回到顶部
-        $('.toTop').click(function(){
+        $('.toTop').click(function () {
             $('body,html').scrollTop(0);
         })
 
@@ -206,35 +206,35 @@
     }
 
     // 定制刻字功能
-    $('.right .dz a').eq(0).click(function(){
-        $('.kz .wrapper .ok').prop('disabled',true).css({'background':'#cccccc','border-color':'#cccccc','cursor':'default'});
+    $('.right .dz a').eq(0).click(function () {
+        $('.kz .wrapper .ok').prop('disabled', true).css({ 'background': '#cccccc', 'border-color': '#cccccc', 'cursor': 'default' });
         $('body').addClass('hidden');
         $('.kz').show();
-        $('.kz input').on('input',function(){
-            var num=8-$(this).val().length;
+        $('.kz input').on('input', function () {
+            var num = 8 - $(this).val().length;
             // console.log(num)
             $('.kz .count').text(num);
-            $('.kz .wrapper .ok').prop('disabled',true);
-            if(num<8) {
-                $('.kz .wrapper .ok').prop('disabled',false).css({'background':'#000000','border-color':'#000000','cursor':'pointer'});
-            }else {
-                $('.kz .wrapper .ok').prop('disabled',true).css({'background':'#cccccc','border-color':'#cccccc','cursor':'default'});
+            $('.kz .wrapper .ok').prop('disabled', true);
+            if (num < 8) {
+                $('.kz .wrapper .ok').prop('disabled', false).css({ 'background': '#000000', 'border-color': '#000000', 'cursor': 'pointer' });
+            } else {
+                $('.kz .wrapper .ok').prop('disabled', true).css({ 'background': '#cccccc', 'border-color': '#cccccc', 'cursor': 'default' });
             }
 
         })
     })
-    $('.kz .ok').on('click',function(){
-        var kznr= $('.kz input').val();
-            $('.dz i.nr').addClass('nrr');
-            $('.dz span.nr').text(kznr).show();
-            // $('.dz span.nr').text(kznr);
-            $('body').removeClass('hidden');
-            $('.kz').hide();
-            $('.kz input').val('');
-            $('.kz .count').text('8');
+    //点击确定键关闭刻字页面，并向刻字区域添加内容
+    $('.kz .ok').on('click', function () {
+        var kznr = $('.kz input').val();
+        $('.dz i.nr').addClass('nrr');
+        $('.dz span.nr').text(kznr).show();
+        $('body').removeClass('hidden');
+        $('.kz').hide();
+        $('.kz input').val('');
+        $('.kz .count').text('8');
     })
     // 关闭定制刻字功能
-    $('.kz .close,.kz  .cancel').on('click',function(e){
+    $('.kz .close,.kz  .cancel').on('click', function (e) {
         // e.stopPropagation();
         $('.kz input').val('');
         $('.kz .count').text('8');
@@ -243,25 +243,28 @@
     })
 
     // 免费送货及退换
-    $('.content .right .btn .th').on('click',function(){
+    $('.content .right .btn .th').on('click', function () {
         $('.tuihuan').show();
         $('body').addClass('hidden');
-        $('.tuihuan span').on('click',function(){
+        $('.tuihuan span').on('click', function () {
             $('.tuihuan').hide();
             $('body').removeClass('hidden');
         })
     })
 
-    //购物车生成之收藏
-    $('.right .btn a').eq(1).click(function(){
-        var isOk=$('.right .color li').hasClass('qq');
-        if(isOk){
-            // $('.right .btn a').eq(1).css('background','#000');
+    //购物车生成之我的收藏
+    $('.right .btn a').eq(1).click(function () {
+        var isOk = $('.right .color li').hasClass('qq');
+        if (isOk) {
             $('.top .car').show();
             //禁止屏幕生成滚动条
             $('body').addClass('hidden');
+            // 显示我的收藏
+            $('.car .down .d1').removeClass('display');
+            // 我的收藏之空替换隐藏
+            $('.car .down .d1').next().addClass('display');
             // 自动生成列表
-            var nr=`
+            var nr = `
             <img src="./upload/${pics[sy].sSrc}" alt="" class="fl">
             <div class="fl m">
                 <p>全新<strong>DIOR</strong>迪奥烈艳蓝金挚红唇膏</p>
@@ -275,38 +278,164 @@
             </div>
             `
             $('<li></li>').appendTo('.car .down .d1').addClass('clearfix').html(nr);
-            $('.top .car .del').click(function(){
+            // 导航栏显示点击效果
+            $('.top .car .up li').eq(0).addClass('active').siblings().removeClass('active');
+            // 购物车收藏之删除功能
+            $('.top .car .d1 .del').click(function () {
                 $(this).parent().parent().remove();
-                var cou=$('.top .car .down .d1').find('li').length;
-                if(cou==0){
+                // li的数量
+                var cou = $('.top .car .down .d1').find('li').length;
+                if (cou == 0) {
                     $('.top .car .up li').eq(0).find('i').text('');
-                }else {
-                    $('.top .car .up li').eq(0).find('i').text('('+cou+')');
+                    // 我的收藏隐藏
+                    $('.car .down .d1').addClass('display');
+                    // 我的收藏之空替换显示
+                    $('.car .down .d1').next().removeClass('display');
+                } else {
+                    // 我的收藏显示
+                    $('.car .down .d1').removeClass('display');
+                    // 我的收藏之空替换隐藏
+                    $('.car .down .d1').next().addClass('display');
+                    $('.top .car .up li').eq(0).find('i').text('(' + cou + ')');
                 }
             })
-            // 关闭购物车
-            $('.top .car .close').on('click',function(){
-                $('body').removeClass('hidden');
-                $('.top .car').hide();
-            })
             //设置ul的高度
-            var diffHeight=$(window).innerHeight()-$('.car .close').height()-$('.car .up').height();
+            var diffHeight = $(window).innerHeight() - $('.car .close').height() - $('.car .up').height();
             $('.top .car .down').height(diffHeight);
+            $('.top .car .down .d2').height(diffHeight);
             //设置切换栏中收藏数量
-            var cou=$('.top .car .down .d1').find('li').length;
-            if(cou==0){
+            var cou = $('.top .car .down .d1').find('li').length;
+            if (cou == 0) {
                 $('.top .car .up li').eq(0).find('i').text('');
-            }else {
-                $('.top .car .up li').eq(0).find('i').text('('+cou+')');
+            } else {
+                $('.top .car .up li').eq(0).find('i').text('(' + cou + ')');
             }
+
+
+
         }
     });
 
-    //购物车之我的账户
-    
+    // 关闭购物车
+    $('.top .car .close').on('click', function () {
+        $('body').removeClass('hidden');
+        $('.top .car').hide();
+    })
+
+    //购物车之购物篮部分
+    $('.right .btn .dg').on('click', function () {
+        //判断是否有选中的商品
+        var isOk = $('.right .color li').hasClass('qq');
+        if (isOk) {
+            // 购物篮显示
+            $('.top .car').show();
+            //禁止屏幕生成滚动条
+            $('body').addClass('hidden');
+            // 购物篮部分显示
+            $('.car .down .d3').removeClass('display');
+            // 购物篮之空替换隐藏
+            $('.car .down .d3').next().addClass('display');
+            // 导航栏显示点击效果
+            $('.top .car .up li').eq(2).addClass('active').siblings().removeClass('active');
+            // 生成购物篮内容
+            var nr = `
+                    <a href="javascript:" class="fl">
+                        <img src="./upload/${pics[sy].cSrc}" alt="">
+                    </a>
+                    <div class="fl basket">
+                        <p>
+                            <a href="javascript:">克丽丝汀迪奥烈艳蓝金挚红唇膏  <strong>${parseInt(datas[sy].num)}</strong></a>
+                        </p>
+                        <span>色泽饱满浓郁，质地轻盈舒悦</span>
+                        <div class="fl left">
+                            <i>￥${datas[sy].price}</i>
+                            <a href="javascript:" class="del">删除</a>
+                        </div>
+                        <div class="fr right">
+                            <i>数量：<em>1</em></i>
+                            <button class="reduce iconfont ccc">&#xe60f;</button>
+                            <button class="add iconfont">&#xe66e;</button>
+                        </div>
+                    </div>
+                    <span class="iconfont love">&#xe82a;</span>
+                    <span class="iconfont display love">&#xe608;</span>
+                `;
+            // var newLi = $('<li class="clearfix"></li>').html(nr)[0];
+            // var lastLi = $('.car .down .d3 .last')[0];
+            // $('.car .down .d3')[0].insertBefore(newLi, lastLi);
+
+            var newLi = $('<li class="clearfix"></li>').html(nr);
+            $('.car .down .d3 .last').before(newLi);
+            // tab切换栏数量更新
+            $('.top .car .up li').eq(2).find('a').text('购物篮(' + d3_count() + ')');
+            // 设置ul高度
+            var diffHeight = $(window).innerHeight() - $('.car .close').height() - $('.car .up').height();
+            $('.top .car .down').height(diffHeight);
+            $('.top .car .down .d3').height(diffHeight);
+            // 购物篮之删除功能
+            $('.top .car .d3 .del').click(function () {
+                $(this).parent().parent().parent().remove();
+                // li的数量
+                var cou = $('.top .car .down .d3').find('li').length;
+                // tab切换栏数量更新
+                $('.top .car .up li').eq(2).find('a').text('购物篮(' + d3_count() + ')');
+                if (cou == 1) {
+                    $('.top .car .up li').eq(2).find('i').text('0');
+                    // 购物篮隐藏
+                    $('.car .down .d3').addClass('display');
+                    // 购物篮之空替换显示
+                    $('.car .down .d3').next().removeClass('display');
+                } else {
+                    // 购物篮显示
+                    $('.car .down .d3').removeClass('display');
+                    // 购物篮之空替换隐藏
+                    $('.car .down .d3').next().addClass('display');
+
+                    // $('.top .car .up li').eq(3).find('i').text('(' + cou + ')');
+                }
+            })
+        }
+    })
+
+    //购物车之购物篮部分点击+，数量加1
+    $('.car .down .d3').on('click', '.add', function () {
+        var cou = parseInt($(this).siblings('i').find('em').text());
+        cou++;
+        $(this).siblings('i').find('em').text(cou);
+        // -按钮取消禁止
+        $(this).prev().removeClass('ccc').css('cursor', 'pointer');
+        // tab切换栏数量更新
+        $('.top .car .up li').eq(2).find('a').text('购物篮(' + d3_count() + ')');
+    });
+    //购物车之购物篮部分点击-，数量减1，并判断是否数量为1
+    $('.car .down .d3').on('click', '.reduce', function () {
+        var cou = parseInt($(this).siblings('i').find('em').text());
+        cou--;
+        if (cou <= 1) {
+            cou = 1;
+            // -按钮添加禁止
+            $(this).addClass('ccc').css('cursor', 'default');
+            $(this).siblings('i').find('em').text(cou);
+            // tab切换栏数量更新
+            $('.top .car .up li').eq(2).find('a').text('购物篮(' + d3_count() + ')');
+        } else {
+            $(this).siblings('i').find('em').text(cou);
+            // tab切换栏数量更新
+            $('.top .car .up li').eq(2).find('a').text('购物篮(' + d3_count() + ')');
+        }
 
 
+    });
 
+    function d3_count() {
+        var counts = $('.car .down .d3 em');
+        var num = 0;
+        for (var i = 0; i < counts.length; i++) {
+            num = num + parseInt(counts.eq(i).text());
+            // console.log(num)
+        }
+        return num;
+    }
 })();
 
 
